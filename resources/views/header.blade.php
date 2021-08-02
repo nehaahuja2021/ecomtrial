3ca95d1605ee65e494cfe25842afcde8330db99c
nehaahuja2021 <nehaahuja2021@gmail.com>
2021-08-02 17:38:50 +0200

    new changes in login

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -16,6 +16,9 @@
         <li class="nav-item">
           <a class="nav-link active" href="#" >Cart(0)</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="/login" >Login</a>
+        </li>
       </ul>
       <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
