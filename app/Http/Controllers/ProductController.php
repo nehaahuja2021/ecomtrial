5a614455b222edd3e00902822de6e341a4915ab1
nehaahuja2021 <nehaahuja2021@gmail.com>
2021-08-13 10:59:08 +0200

    login/registraion/all links in json for end point API

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -18,9 +18,9 @@ class ProductController extends Controller
         /* storing data in variable and passing it to view with an array named products.*/
                 //return view ('product',['products'=>$data]);One way
 //////////////////////
-       return view ('product')->with('products',product::all());
+      // return view ('product')->with('products',product::all());
       
-      //return response()->json($data);
+      return response()->json($data);
         
       
     }
@@ -34,10 +34,10 @@ class ProductController extends Controller
       $db_output=DB::table('products')->where('name', 'like' ,'%' .$user_input. '%')->get();
       //return response()->json($db_output);  
    
-         return view ('search')->with('productArr',product::where('name', 'like' ,'%' .$user_input. '%')->get());  
+         //return view ('search')->with('productArr',product::where('name', 'like' ,'%' .$user_input. '%')->get());  
            
 
-        // return response()->json(['data' => $db_output->toArray()], 201);
+         return response()->json(['data' => $db_output->toArray()], 201);
 }
 
 
@@ -67,7 +67,7 @@ function add_to_cart(request $req)
 //return \View::make('/yourcart')->with('cart_count', $cart_count);
 
 // slash before view as view class is not included.
-      //return response()->json($cart_count);
+      return response()->json($cart_count);
     
       }
 
@@ -81,8 +81,8 @@ $prod_display=DB::table('cart')
 ->select('products.*','cart.id as cart_id')
 ->get();
 //echo "$prod_display";
-return view('yourcart',['productsdisplay'=>$prod_display]);
-//return response()->json($prod_display);
+//return view('yourcart',['productsdisplay'=>$prod_display]);
+return response()->json($prod_display);
 
 }
 
@@ -92,7 +92,7 @@ function vegetables()
     $data_veggies = DB::table('products')
     ->whereIn('category_id', [1])
     ->get();
-    //return response()->json(['vegetables' => $data_veggies->toArray()], 201);
+    return response()->json(['vegetables' => $data_veggies->toArray()], 201);
 }
 
 function fruits()
@@ -123,8 +123,9 @@ function order_now()
     
     //return view('ordernow',['productsorder'=>$prod_order]);
 
-    return Response::json(['ordernow' => $prod_order]);
-
+   return Response::json(['ordernow' => $prod_order]);
+   //return view('ordernow')
+   //->with('ordernow', $prod_order);
 
 }
 
